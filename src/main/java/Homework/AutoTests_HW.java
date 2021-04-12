0c17ec26fe619393ace94252453224aeb65f3b08
MosiichukO <Collegium12>
2021-04-12 18:27:24 +0300

    update

--- src/main/java/Homework/AutoTests_HW.java
+++ src/main/java/Homework/AutoTests_HW.java
@@ -1,7 +1,6 @@
 package Homework;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,39 +12,39 @@ import static Homework.Attributes.*;
 
 
 public class AutoTests_HW {
+
+    @Before
+    public void setUp() {
+        Singleton.initialize();
+    }
+
+
     @Test
-    public  void test_checkFunctional () {
+    public void test_checkFunctional() {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-
-        driver.get(URL_SITE);               // 3.1
-
-        WebElement randomName = driver.findElement(By.id(ID_RANDOM_NAME));      // 3.2
+        WebElement randomName = Singleton.driver.findElement(By.id(ID_RANDOM_NAME));      // 3.2
         randomName.click();
 
-        driver.findElement(By.id(ID_DOMAIN)).click();           // 3.3
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+        Singleton.driver.findElement(By.id(ID_DOMAIN)).click();           // 3.3
+        WebDriverWait wait = new WebDriverWait(Singleton.driver, 10);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.
                 xpath(XPATH_DOMAIN_ROVERINFO)))
                 .click();
 
-        String nameOfEmail = driver.findElement(By.id(ID_EMAIL_NAME))  // 3.4
+        String nameOfEmail = Singleton.driver.findElement(By.id(ID_EMAIL_NAME))  // 3.4
                 .getAttribute("value") + EMAIL_DOMAIN;
 
-        WebElement settings = driver.findElement(By.id(ID_SETTINGS)); // 3.5
+        WebElement settings = Singleton.driver.findElement(By.id(ID_SETTINGS)); // 3.5
         settings.click();
 
 
 //        wait.until(ExpectedConditions.elementToBeClickable(By.
 //                xpath(XPATH_TEN_MINUTES_BUTTON))).click();
-//        driver.findElement(By.xpath(SAVE_BUTTON)).click();
+//        Singleton.driver.findElement(By.xpath(SAVE_BUTTON)).click();
 //
 //        settings.click();
 //
-//        String checkActive = driver.findElement((By.
+//        String checkActive = Singleton.driver.findElement((By.
 //                xpath(XPATH_TEN_MINUTES_BUTTON))).getAttribute("class");
 //       Assert.assertTrue(checkActive.contains("active"));
 
@@ -59,22 +58,22 @@ public class AutoTests_HW {
         Assert.assertTrue(randomName.isDisplayed());
 
 
-        WebElement waitingLetters = driver.findElement(By.xpath(XPATH_WAITING_LETTERS_TEXT));     // 3.8
-        String waitingLettersString = driver.findElement(By.xpath(XPATH_WAITING_LETTERS_TEXT)).getAttribute("textContent");
+        WebElement waitingLetters = Singleton.driver.findElement(By.xpath(XPATH_WAITING_LETTERS_TEXT));     // 3.8
+        String waitingLettersString = Singleton.driver.findElement(By.xpath(XPATH_WAITING_LETTERS_TEXT)).getAttribute("textContent");
         Assert.assertEquals("В ожидании новых писем...", waitingLettersString);
         Assert.assertTrue(waitingLetters.isDisplayed());
 
 
-        driver.findElement(By.xpath(XPATH_WRITE_BUTTON)).click(); // 3.9
+        Singleton.driver.findElement(By.xpath(XPATH_WRITE_BUTTON)).click(); // 3.9
 
-        WebElement sendButton = driver.findElement(By.xpath(XPATH_SEND_BUTTON)); // 3.10
+        WebElement sendButton = Singleton.driver.findElement(By.xpath(XPATH_SEND_BUTTON)); // 3.10
         wait.until(ExpectedConditions.visibilityOf(sendButton));
         Assert.assertTrue(sendButton.isDisplayed());
-        driver.findElement(By.xpath(XPATH_LETTER_TO))
+        Singleton.driver.findElement(By.xpath(XPATH_LETTER_TO))
                 .sendKeys(nameOfEmail);
-        driver.findElement(By.xpath(XPATH_LETTER_THEME))
+        Singleton.driver.findElement(By.xpath(XPATH_LETTER_THEME))
                 .sendKeys("Test");
-        driver.findElement(By.xpath(XPATH_LETTER_BODY))
+        Singleton.driver.findElement(By.xpath(XPATH_LETTER_BODY))
                 .sendKeys(secretAddress);
 
 
@@ -84,24 +83,24 @@ public class AutoTests_HW {
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath(XPATH_NEW_LETTER_INCOME))).click(); // 3.12
 
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath(XPATH_REPLY_BUTTON))); // 3.13
-        WebElement replyButton = driver.findElement(By.xpath(XPATH_REPLY_BUTTON));
+        WebElement replyButton = Singleton.driver.findElement(By.xpath(XPATH_REPLY_BUTTON));
 
-        String senderName = driver.findElement(By.xpath(XPATH_SENDER_EMAIL))
+        String senderName = Singleton.driver.findElement(By.xpath(XPATH_SENDER_EMAIL))
                 .getAttribute("textContent");
         Assert.assertEquals(nameOfEmail, senderName);
 
-        String themeOfEmail = driver.findElement(By.xpath(XPATH_THEME_INCOME_LETTER))
+        String themeOfEmail = Singleton.driver.findElement(By.xpath(XPATH_THEME_INCOME_LETTER))
                 .getAttribute("textContent");
         Assert.assertEquals("Test", themeOfEmail);
 
-        String textOfEmail = driver.findElement(By.xpath(XPATH_BODY_INCOME_LETTER))
+        String textOfEmail = Singleton.driver.findElement(By.xpath(XPATH_BODY_INCOME_LETTER))
                 .getAttribute("textContent");
         Assert.assertEquals(secretAddress, textOfEmail);
 
 
         replyButton.click();                                                    // 3.14
         wait.until(ExpectedConditions.visibilityOf(sendButton));
-        driver.findElement(By.xpath(XPATH_LETTER_BODY)).sendKeys("Test2");
+        Singleton.driver.findElement(By.xpath(XPATH_LETTER_BODY)).sendKeys("Test2");
         sendButton.click();
 
 
@@ -109,30 +108,34 @@ public class AutoTests_HW {
                 elementToBeClickable(By.xpath(XPATH_BACK_BUTTON))).click();
 
 
-
         String resendEmail = "Re: " + themeOfEmail;  // 3.16
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(XPATH_DELETE_ALL)));
 
-        String toCheckRe = driver.findElement(By.xpath(XPATH_THEME_INCOME_LETTER_MAINPAGE))
+        String toCheckRe = Singleton.driver.findElement(By.xpath(XPATH_THEME_INCOME_LETTER_MAINPAGE))
                 .getAttribute("textContent");
         Assert.assertEquals(resendEmail, toCheckRe);
 
-        driver.findElement(By.xpath(XPATH_INCOME_LETTER_MAINPAGE)).click();
+        Singleton.driver.findElement(By.xpath(XPATH_INCOME_LETTER_MAINPAGE)).click();
 
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(XPATH_BODY_INCOME_LETTER)));  // 3.17
 
-        String textOfEmailRe = driver.findElement(By.xpath(XPATH_BODY_INCOME_LETTER)).getAttribute("textContent");
+        String textOfEmailRe = Singleton.driver.findElement(By.xpath(XPATH_BODY_INCOME_LETTER)).getAttribute("textContent");
         Assert.assertEquals("Test2", textOfEmailRe);
 
 
-        driver.findElement(By.xpath(XPATH_DELETE_LETTER_BUTTON)).click(); // 3.18
+        Singleton.driver.findElement(By.xpath(XPATH_DELETE_LETTER_BUTTON)).click(); // 3.18
         wait.until(ExpectedConditions
                 .elementToBeClickable(By.xpath(XPATH_CONFIRM_DELETE_LETTER_BUTTON))).click();
 
 
         wait.until(ExpectedConditions                                      // 3.19
                 .visibilityOfElementLocated(By.xpath(XPATH_DELETE_ALL)));
-        Assert.assertFalse(driver.getPageSource().contains("Re: Test"));
+        Assert.assertFalse(Singleton.driver.getPageSource().contains("Re: Test"));
+    }
+
+    @After
+    public void finish() {
+        Singleton.quit();
     }
 }
---
+++ src/main/java/Homework/Singleton.java
@@ -0,0 +1,33 @@
+package Homework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static Homework.Attributes.URL_SITE;
+
+public class Singleton {
+
+    public static WebDriver driver = null;
+    public static String browserName = "chrome";
+
+    public static void initialize () {
+        if (driver == null) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver_win32\\chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(URL_SITE);
+    }
+
+    public static void quit () {
+        driver.quit();
+        driver = null;
+    }
+
+}
